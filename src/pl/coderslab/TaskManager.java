7f0e0c54e81565f525fcf3f4467c7a78b94b2951
emaslanka <ewe.maslanka@gmail.com>
2020-12-02 22:53:24 +0100

    case,scan add

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -16,14 +16,39 @@ public class TaskManager {
         //Viewing all tasks available
         ViewofTasks();
 
-        //REad a file and saving datas in 2D array
-        try {
-            readFile();
-        } catch (IOException E) {
-            E.printStackTrace();
+
+        Scanner choseOne = new Scanner(System.in);
+        String Input = choseOne.nextLine();  // User can choose one task
+
+        switch (Input) {
+            case "Add": {
+                Addtask();
+                break;
+            }
+            case "List": {
+                //REad a file and saving datas in 2D array, check if file exists
+                try {
+                    readFile();
+                } catch (IOException E) {
+                    E.printStackTrace();
+                }
+                break;
+            }
+            case "Exit": {
+                System.out.println(ConsoleColors.PURPLE+"Bye bye. I hope to see you again");
+                break;
+            }
+            default: {
+                System.out.println(ConsoleColors.RED + "PLease, select correct option");
+            }
         }
 
+        choseOne.close();
+
+
     }
+
+
 
     public static void ViewofTasks() {
         System.out.println(ConsoleColors.BLUE + "Please, select an option :");
@@ -34,6 +59,7 @@ public class TaskManager {
             System.out.println(ConsoleColors.WHITE + tasksName);
         }
     }
+
 
 
 
@@ -58,13 +84,32 @@ public class TaskManager {
             String[][] TasksDatesIMp = new String[Lines.size()][];  // create 2D array [lines in LIst]
             for (int j = 0; j < TasksDatesIMp.length; j++) {        // every row is fulfilled with one line of list = 1d array
                 TasksDatesIMp[j] = Lines.get(j);
+                System.out.println(j + " " + Arrays.toString(TasksDatesIMp[j]));//LIst of tasks is viewed
             }
 
-            System.out.println(Arrays.deepToString(TasksDatesIMp));  // List of tasks is viewed
 
         } catch (IOException E) {
             E.printStackTrace();
         }
 
     }
+
+
+
+    public static void Addtask()
+    {
+        Scanner add = new Scanner(System.in);
+        System.out.println("Please add task description :");
+        String descripton = add.nextLine();
+        System.out.println("PLease add task due date YYYY-MM-DD:");
+        String date = add.nextLine();
+        System.out.println("Is your task important?  true/false");
+        String importance = add.nextLine();
+
+
+        add.close();
+
+
+    }
+
 }
