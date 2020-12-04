9ca3a43daee7ccc9f11ec3f93e1e1342ac900e06
emaslanka <ewe.maslanka@gmail.com>
2020-12-05 00:32:30 +0100

    Add+remove

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,6 +1,11 @@
 package pl.coderslab;
 
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,31 +16,41 @@ import java.util.Scanner;
 
 public class TaskManager {
 
+
     public static void main(String[] args) {
 
-        //Viewing all tasks available
         ViewofTasks();
-
-
         Scanner choseOne = new Scanner(System.in);
         String Input = choseOne.nextLine();  // User can choose one task
 
+        String[][] TasksDatesIMp = getTableStrings();                                                                  // Reading table from file, and getting it to 2D Table
+
+
         switch (Input) {
             case "Add": {
-                Addtask();
+                String[][] tab = Addtask(TasksDatesIMp);
+                SavetoFile(tab);
                 break;
             }
             case "List": {
-                //REad a file and saving datas in 2D array, check if file exists
-                try {
+                try {                                                                                                   //REad a file and saving datas in 2D array, check if file exists
                     readFile();
                 } catch (IOException E) {
                     E.printStackTrace();
                 }
                 break;
             }
+
+
+            case "Remove": {                                                                                             //Removing task
+                System.out.println("Please select number to remove ?");
+                String number = choseOne.nextLine();
+                RemoveTask(TasksDatesIMp, number);
+                break;
+            }
+
             case "Exit": {
-                System.out.println(ConsoleColors.PURPLE+"Bye bye. I hope to see you again");
+                System.out.println(ConsoleColors.PURPLE + "Bye bye. I hope to see you again");
                 break;
             }
             default: {
@@ -44,10 +59,58 @@ public class TaskManager {
         }
 
         choseOne.close();
-
-
     }
 
+
+    //Methods
+    public static String[][] getTableStrings() {
+
+        Path path1 = Paths.get("tasks.csv");
+        List<String[]> Lines = new ArrayList<>();                                                                       //List to collect lines from file
+        String[][] TasksDatesIMp = {};
+
+        try {
+            Scanner scan = new Scanner(path1);
+            String line;
+
+            while (scan.hasNextLine()) {
+                line = scan.nextLine();
+                String[] splited = line.split(", ");                                                              // save splited by ", " line to new 1d array
+                Lines.add(splited);                                                                                     // 1d array is added to List
+            }
+
+            TasksDatesIMp = new String[Lines.size()][];                                                                 // create 2D array [lines in LIst]
+            for (int j = 0; j < TasksDatesIMp.length; j++) {                                                            // every row is fulfilled with one line of list = 1d array
+                TasksDatesIMp[j] = Lines.get(j);
+            }
+
+        } catch (IOException E) {
+            E.printStackTrace();
+        }
+        return TasksDatesIMp;
+    }
+
+
+    public static void SavetoFile(String[][] tab) {
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].length; j++) {
+                builder.append(tab[i][j] + ", ");
+            }
+            builder.append("\n");
+        }
+
+        try {                                                                                                         //Saving to file
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("tasks.csv"));
+            writer.write(builder.toString());
+            writer.close();
+
+        } catch (IOException E) {
+            E.printStackTrace();
+        }
+    }
 
 
     public static void ViewofTasks() {
@@ -65,39 +128,15 @@ public class TaskManager {
 
     public static void readFile() throws IOException {
 
-        Path path1 = Paths.get("tasks.csv");
-
-        //List to collect lines from file
-        List<String[]> Lines = new ArrayList<>();
-
-
-        try {
-            Scanner scan = new Scanner(path1);
-            String line;
-
-            while (scan.hasNextLine()) {
-                line = scan.nextLine();
-                String[] splited = line.split(", ");        // save splited by ", " line to new 1d array
-                Lines.add(splited);                               // 1d array is added to List
-            }
-
-            String[][] TasksDatesIMp = new String[Lines.size()][];  // create 2D array [lines in LIst]
-            for (int j = 0; j < TasksDatesIMp.length; j++) {        // every row is fulfilled with one line of list = 1d array
-                TasksDatesIMp[j] = Lines.get(j);
-                System.out.println(j + " " + Arrays.toString(TasksDatesIMp[j]));//LIst of tasks is viewed
-            }
-
-
-        } catch (IOException E) {
-            E.printStackTrace();
+        String[][] TabletoRead = getTableStrings();
+        for (int i = 0; i < TabletoRead.length; i++) {
+            System.out.println(i + 1 + " " + Arrays.toString(TabletoRead[i]));
         }
-
     }
 
 
+    public static String[][] Addtask(String[][] TasksDatesIMp) {
 
-    public static void Addtask()
-    {
         Scanner add = new Scanner(System.in);
         System.out.println("Please add task description :");
         String descripton = add.nextLine();
@@ -106,10 +145,42 @@ public class TaskManager {
         System.out.println("Is your task important?  true/false");
         String importance = add.nextLine();
 
+        String[][] newTaskDatesImp = new String[TasksDatesIMp.length + 1][TasksDatesIMp[0].length];
+
+        for (int i = 0; i < newTaskDatesImp.length - 1; i++) {
+
+            System.arraycopy(TasksDatesIMp[i], 0, newTaskDatesImp[i], 0, TasksDatesIMp[i].length);
+
+        }
+
+        newTaskDatesImp[newTaskDatesImp.length - 1][0] = descripton;                                                     //inserting new values to
+        newTaskDatesImp[newTaskDatesImp.length - 1][1] = date;
+        newTaskDatesImp[newTaskDatesImp.length - 1][2] = importance;
 
         add.close();
+
+        SavetoFile(newTaskDatesImp);
+        return newTaskDatesImp;
 
 
     }
 
+    public static void RemoveTask(String[][] TasksDatesIMp, String number) {
+
+
+        if (NumberUtils.isParsable(number) && Integer.parseInt(number) > 0) {
+            if (Integer.parseInt(number) <= TasksDatesIMp.length) {
+
+                int numbertoDel = Integer.parseInt(number) - 1;
+                TasksDatesIMp = ArrayUtils.remove(TasksDatesIMp, numbertoDel);
+            } else {
+                System.out.println("Value is greater than amount of tasks.");
+            }
+        } else {
+            System.out.println("Incorrect value. Try again");
+        }
+
+        SavetoFile(TasksDatesIMp);
+
+    }
 }
